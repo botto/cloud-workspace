a9e6a29afde9b377e4b036acb056c9d25d2420de
Martin Eskdale Moen <martinmoen@gmail.com>
2016-02-04 13:55:21 +0000

    Some simple variable substitutions

--- workspace.tf
+++ workspace.tf
@@ -1,15 +1,14 @@
 # Configure the Google Cloud provider
 provider "google" {
   credentials = "${file("gc_credentials.json")}"
-  project     = ""
-  region      = ""
-  account_file = "${file("gc_credentials.json")}"
+  project     = "${var.gc.project}"
+  region      = "${var.gc.region}"
 }
 
 resource "google_compute_instance" "workspace" {
   name = "workspace"
   machine_type = "n1-standard-1"
-  zone = "europe-west1-b"
+  zone = "${var.gc.region}"
 
   disk {
     image = "ubuntu-1510-wily-v20151114"
