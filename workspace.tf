7dd45987b1c51d91d769002e33497e92332b6fa5
Martin Eskdale Moen <martinmoen@gmail.com>
2016-02-07 12:16:54 +0000

    Configure GC network for custom firewall rules

--- workspace.tf
+++ workspace.tf
@@ -5,6 +5,39 @@ provider "google" {
   region      = "${var.gc.region}"
 }
 
+resource "google_compute_network" "workspace" {
+  name = "workspace"
+  ipv4_range = "10.0.100.0/24"
+}
+
+resource "google_compute_firewall" "workspace-mosh" {
+  name = "allow-mosh"
+  network = "${google_compute_network.workspace.name}"
+
+  allow {
+    protocol = "udp"
+    ports = ["60000-61000"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "google_compute_firewall" "workspace-ssh" {
+  name = "allow-ssh"
+  network = "${google_compute_network.workspace.name}"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+}
+
 resource "google_compute_instance" "workspace" {
   name = "workspace"
   machine_type = "n1-standard-1"
@@ -15,9 +48,8 @@ resource "google_compute_instance" "workspace" {
   }
 
   network_interface {
-    network = "default"
+    network = "${google_compute_network.workspace.name}"
     access_config {
     }
   }
 }
-
