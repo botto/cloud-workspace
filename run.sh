5d0e092da956eac67abd10e38619f98ce49ca742
Martin Eskdale Moen <martinmoen@gmail.com>
2016-02-15 17:58:38 +0000

    Fixed issue with reruns destroying the state
    Looks like when you run terraform apply using a plan that has already
    been run before it destroys the state, a bit strange but ok, we can work
    our way around this.

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+trap 'if [ -e workspace_plan.temp ]; then rm workspace_plan.temp; fi' EXIT
+
 terraform plan -out workspace_plan.temp
-terraform apply workspace_plan.temp
+if [ -e workspace_plan.temp ]; then
+  terraform apply workspace_plan.temp
+fi
 ANSIBLE_HOST_KEY_CHECKING=False env ansible-playbook -i terraform-inventory ansible/run.yml
+
+exit 0
